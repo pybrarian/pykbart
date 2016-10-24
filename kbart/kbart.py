62a12de9a182f1cc4a06a5a577a5768c2bb38a96
Ed Hill <hill.charles2@gmail.com>
2016-10-24 09:45:04 -0400

    Adding doc strings and fixing some small format issues in kbart

--- kbart/kbart.py
+++ kbart/kbart.py
@@ -3,7 +3,7 @@
 
 from __future__ import (absolute_import, division,
                         print_function, unicode_literals)
-from collections import OrderedDict
+from collections import OrderedDict, MutableMapping
 import datetime
 import re
 
@@ -16,7 +16,7 @@ from kbart.exceptions import (InvalidRP, ProviderNotFound,
 
 
 @six.python_2_unicode_compatible
-class Kbart:
+class Kbart(MutableMapping):
     """Kbart representation without having to remember field positions."""
 
     def __init__(self,
@@ -59,13 +59,17 @@ class Kbart:
                                                              fillvalue=''))
 
     def __getitem__(self, key):
+        """Delegate most work to the OrderedDict held by class."""
         return self._kbart_data[key]
 
     def __setitem__(self, key, value):
         self._kbart_data[key] = value
 
+    def __delitem__(self, key):
+        del(self._kbart_data[key])
+
     def __repr__(self):
-        return ("{0}(data={1}, provider={2}, rp={3}, fields={4})\n"
+        return ('{0}(data={1}, provider={2}, rp={3}, fields={4})\n'
                 .format(self.__class__.__name__,
                         self.data,
                         self.provider,
@@ -73,20 +77,23 @@ class Kbart:
                         self.fields))
 
     def __str__(self):
-        output = [" -------\n"]
+        output = [' -------\n']
         output.extend([_format_strings(the_string=self._kbart_data[key],
-                                       prefix="{0}: ".format(key),
-                                       suffix="\n")
+                                       prefix='{0}: '.format(key),
+                                       suffix='\n')
                       for key in self._kbart_data])
-        return "".join(output)
+        return ''.join(output)
 
     def __len__(self):
         return len(self._kbart_data)
 
+    def __iter__(self):
+        return self._kbart_data.__iter__()
+
     def get_fields(self, *args):
         """Get values for the listed keys."""
         if not args:
-            return self._kbart_data.values()
+            return list(self._kbart_data.values())
 
         return [self._kbart_data[x] for x in args
                 if x in self._kbart_data]
@@ -153,14 +160,30 @@ class Kbart:
         return fields
 
 
-class Holdings:
+class Holdings(object):
+    """
+    Essentially just a namespace class for holdings logic so the class meant
+    to represent a KBART record doesn't also need to know how to think about
+    holdings data and can delegate instead.
+    """
     @staticmethod
     def pretty_print(holdings):
+        """
+        Make holdings information more readable to a human.
+
+        Args:
+            holdings: Iterable of the 6 KBART holdings fields.
+                Slice [:3] are the first holding year, vol, issue
+                Slice [3:] are the last year, vol, issue
+
+        Returns:
+            String representing the holdings or a message that no holdings
+            exist.
+        """
         if any(holdings):
-            first, last = holdings[:3], holdings[3:]
-            first_holding = Holdings._create_holdings_string(first)
-            last_holding = Holdings._create_holdings_string(last)
-            if not last_holding:
+            first_holding = Holdings._create_holdings_string(holdings[:3])
+            last_holding = Holdings._create_holdings_string(holdings[3:])
+            if not last_holding:  # KBART logic, no last holding = to present
                 last_holding = 'present'
             return '{0} - {1}'.format(first_holding, last_holding)
         else:
@@ -195,6 +218,7 @@ class Holdings:
     def _create_holdings_string(holdings):
         """
         Format a section of KBART holdings into a human-readable string.
+
         Args:
             holdings: 3 element list; [date, vol, issue] following KBART order
 
@@ -204,25 +228,58 @@ class Holdings:
         """
         holdings[1] = _format_strings(holdings[1], prefix='Vol: ')
         holdings[2] = _format_strings(holdings[2], prefix='Issue: ')
-        return ', '.join([x for x in holdings if x])
+        return ', '.join(x for x in holdings if x)
 
 
-class Embargo:
+class Embargo(object):
+    """
+    Essentially just a namespace class for embargo logic so the class meant
+    to represent a KBART record doesn't also need to know how to think about
+    embargo data and regexes etc. and can delegate instead.
+    """
+
+    # using named groups, so ?P<type> etc. are group names rather than
+    # matching logic
     pattern = re.compile('(?P<type>R|P)(?P<length>\d+)(?P<unit>D|M|Y)')
 
     @staticmethod
     def pretty_print(embargo):
+        """
+        Change an embargo code, i.e. R1Y, to a human-readable string, i.e.
+        'Up to 1 year ago'.
+
+        Args:
+            embargo: A KBART embargo string, i.e. R1Y
+        Returns:
+            A string stating the embargo information.
+            KeyError is thrown if no embargo is passed and _embargo_as_dict
+            returns a blank object, and in that case return empty string.
+        """
         embargo_parts = Embargo._embargo_as_dict(embargo)
         try:
             type_of_embargo = EMBARGO_CODES_TO_STRINGS[embargo_parts['type']]
-            length = embargo_parts['length']
             units = EMBARGO_CODES_TO_STRINGS[embargo_parts['unit']]
-            return type_of_embargo.format(length, units)
-        except KeyError as e:
+            return type_of_embargo.format(embargo_parts['length'], units)
+        except KeyError:
             return ''
 
     @staticmethod
     def _embargo_as_dict(embargo):
+        """
+        Take an embargo, break it up with the class level regex, and make
+        it into a regular dict
+        Args:
+            embargo: A KBART embargo string, i.e. R1Y
+
+        Returns:
+            A dict containing the embargo sections, or an empty dict if no
+            embargo. Empty dict is essentially Null Object pattern to avoid
+            continuous error handling or existence checks when reading a
+            KBART file.
+
+        Raises:
+            UnknownEmbargoFormat: If an embargo is passed but can't be parsed
+        """
         if embargo:
             try:
                 embargo_parts = Embargo.pattern.match(embargo)
@@ -235,6 +292,7 @@ class Embargo:
 
 
 def _format_strings(the_string='', prefix='', suffix=''):
+    """Small convenience function, allows easier logic in .format() calls"""
     if the_string:
         return '{0}{1}{2}'.format(prefix, the_string, suffix)
     else:
