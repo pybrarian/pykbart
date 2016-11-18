14016e3d88c358ed1789f07f9822deaf36ca56fd
Ed Hill <hill.charles2@gmail.com>
2016-11-18 14:59:04 -0500

    Changed Reader back to Iterator class so fields instance attribute would be easily available

--- pykbart/reader.py
+++ pykbart/reader.py
@@ -11,17 +11,23 @@ from pykbart.kbart import Kbart
 import unicodecsv as csv
 
 
-def kbart_reader(file_handle, delimiter='\t'):
-    reader = csv.reader(file_handle, delimiter=delimiter, encoding='utf-8')
-    field_names = list(six.next(reader))
-    for row in reader:
-        yield Kbart(row, fields=field_names)
+class Reader(six.Iterator):
+
+    def __init__(self, file_handle, delimiter='\t'):
+        self.reader = csv.reader(file_handle, delimiter=delimiter, encoding='utf-8')
+        self.fields = list(six.next(self.reader))
+
+    def __next__(self):
+        return Kbart(six.next(self.reader), fields=self.fields)
+
+    def __iter__(self):
+        return self
 
 
 @contextlib.contextmanager
 def KbartReader(file_path, delimiter='\t'):
     f = open(file_path, 'rb')
     try:
-        yield kbart_reader(f, delimiter=delimiter)
+        yield Reader(f, delimiter=delimiter)
     finally:
         f.close()
